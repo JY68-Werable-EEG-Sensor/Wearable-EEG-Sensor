a2e0e5d8150208120812924779fa260bf422b295
Nillza <christopherwang0@gmail.com>
2023-11-24 23:11:53 -0800

    send randomized json data to s3 bucket

--- webapp_files/P1/gateway.py
+++ webapp_files/P1/gateway.py
@@ -1,21 +1,25 @@
 import boto3
 import json
+# temporary random dict
+# to be replaced with bluetooth data
+import random
+
+def generate_random_dict(size):
+    random_dict= {}
+
+    for i in range(size):
+        key = i
+        voltage = round(random.uniform(-3.3, 3.3), 3)
+        random_dict[key] = voltage;
+
+    return random_dict
 
 # create S3 client
 s3 = boto3.client("s3")
 
 # dictionary
-my_data = {
-    'text': {
-        "text1": "hello",
-        "text2": "world",
-        "text3": "test aws s3",
-    },
-    'user': {
-        'name': "chris",
-        'time': '10'
-    }
-}
+my_data = generate_random_dict(10)
+
 my_data_json_str = json.dumps(my_data)
 
 print(json.dumps(my_data))
